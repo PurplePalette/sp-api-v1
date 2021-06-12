5befc3189ac76cb12564dd4143d60fd7e29fab8b
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-06-13 00:54:47 +0900

    🚨 Fix lint warning of validation.go

--- utils/request/validation.go
+++ utils/request/validation.go
@@ -2,6 +2,7 @@ package request
 
 import "regexp"
 
+// IsValidName validates the name is only number and alphabets.
 func IsValidName(name string) bool {
 	isAlpha := regexp.MustCompile(`^[0-9a-zA-Z]+$`).MatchString
 	return isAlpha(name)
