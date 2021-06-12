636238c136447c407290f4590bac45c74a6b0766
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-06-13 01:59:17 +0900

    🚨 Fix lint warning of model_cache.go

--- potato/model_cache.go
+++ potato/model_cache.go
@@ -10,19 +10,24 @@ import (
 	"github.com/PurplePalette/sonolus-uploader-core/utils/request"
 )
 
+// Cache is data store struct for storeing data
 type Cache struct {
 	Data map[string]interface{}
 }
 
+// IsExist check the specified key is exists in cache
 func (c *Cache) IsExist(key string) bool {
 	_, ok := c.Data[key]
 	return ok
 }
 
+// Set sets the value to cache with using specified key
 func (c *Cache) Set(key string, value interface{}) {
 	c.Data[key] = value
 }
 
+// Get gets value from cache with using specified key.
+// It returns error if key does not exist in cache.
 func (c *Cache) Get(key string) (interface{}, error) {
 	value, ok := c.Data[key]
 	if !ok {
@@ -31,24 +36,29 @@ func (c *Cache) Get(key string) (interface{}, error) {
 	return value, nil
 }
 
-func (c *Cache) Add(key string, data interface{}) error {
+// Add adds the value to cache with using specified key.
+// It returns error if key already exists.
+func (c *Cache) Add(key string, value interface{}) error {
 	_, ok := c.Data[key]
 	if ok {
 		return errors.New("specified key already exists")
 	}
-	c.Data[key] = data
+	c.Data[key] = value
 	return nil
 }
 
+// Remove removes data from cache that has specified key.
 func (c *Cache) Remove(key string, data interface{}) error {
 	_, ok := c.Data[key]
-	if ok {
-		return errors.New("specified key already exists")
+	if !ok {
+		return errors.New("specified key not exists")
 	}
 	delete(c.Data, key)
 	return nil
 }
 
+// IsOwnerMatch check the owner of specified content is same as specified userID.
+// It returns error if specified key was not existed.
 func (c *Cache) IsOwnerMatch(key string, userID string) (bool, error) {
 	v, ok := c.Data[key]
 	if !ok {
@@ -72,12 +82,14 @@ func (c *Cache) IsOwnerMatch(key string, userID string) (bool, error) {
 	}
 }
 
+// Pages returns the length of cache divided by 20 and also roundup proceeded
 func (c *Cache) Pages() int32 {
 	dataLen := float64(len(c.Data))
 	return int32(math.Ceil(dataLen / 20))
 }
 
-func Paginate(pageNum int32, pageSize int, sliceLength int) (int, int) {
+// paginate return start of page, and end of page, used for getting specified page
+func paginate(pageNum int32, pageSize int, sliceLength int) (int, int) {
 	start := int(pageNum) * pageSize
 
 	if start > sliceLength {
@@ -92,7 +104,10 @@ func Paginate(pageNum int32, pageSize int, sliceLength int) (int, int) {
 	return start, end
 }
 
-func (c *Cache) GetPage(pageId int32, searchQuery request.SearchQuery) ([]byte, error) {
+// GetPage gets specified page from cache datalist.
+// 20 contents will be returned as json bytes array if succeed.
+// It returns error if it couldn't marshal to json bytes array.
+func (c *Cache) GetPage(pageID int32, searchQuery request.SearchQuery) ([]byte, error) {
 	// Convert map to slice
 	var dataList DataList
 	for _, value := range c.Data {
@@ -174,7 +189,7 @@ func (c *Cache) GetPage(pageId int32, searchQuery request.SearchQuery) ([]byte, 
 			return nil, errors.New("user data pagination is not supported yet")
 		}
 	}
-	start, end := Paginate(pageId, 20, len(filteredList))
+	start, end := paginate(pageID, 20, len(filteredList))
 	jsonData, err := json.Marshal(filteredList[start:end])
 	if err != nil {
 		return nil, err
